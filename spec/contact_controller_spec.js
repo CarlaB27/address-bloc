386da9c6721dd9faa7cb175e00210d65986dd809
Carla <carla.f.bates@hotmail.com>
2019-02-04 21:45:08 -0500

    assignment-3

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -22,6 +22,7 @@ describe("#addContact()", () => {
             .then((contact) => {
                 expect(contact.name).toBe("Alice");
                 expect(contact.phone).toBe("001-101-1010");
+                expect(contact.email).toBe("alice@alice.com");
                 done();
             })
             .catch((err) => {
