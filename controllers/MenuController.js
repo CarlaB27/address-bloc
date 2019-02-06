2280479f4e87f1ff27cb6f19f508e84b12438287
Carla <carla.f.bates@hotmail.com>
2019-02-05 22:32:13 -0500

    Listing Contacts and Searching checkpoint

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,6 +10,8 @@ module.exports = class MenuController {
                 message: "Please choose from an option below: ",
                 choices: [
                     "Add new contact",
+                    "View all contacts",
+                    "Search for a contact",
                     "Date",
                     "Exit"
                 ]
@@ -27,6 +29,12 @@ module.exports = class MenuController {
                     break;
                 case "Date":
                     this.getDate();
+                    break;
+                case "View all contacts":
+                    this.getContacts();
+                    break;
+                case "Search for a contact":
+                    this.search();
                     break;
                 case "Exit":
                     this.exit();
@@ -62,11 +70,6 @@ module.exports = class MenuController {
         process.exit();
     }
 
-<<<<<<< HEAD
-    getContactCount() {
-        return this.contacts.length;
-    }
-=======
     getDate() {
         this.clear();
         const date = new Date();
@@ -74,7 +77,89 @@ module.exports = class MenuController {
         this.main();
     }
 
+    getContacts() {
+        this.clear();
+        this.book.getContacts().then((contacts) => {
+            for (let contact of contacts) {
+                console.log(`
+                name: ${contact.name}
+                phone number: ${contact.phone}
+                email: ${contact.email}
+                ---------------`
+                );
+            }
+            this.main();
+        }).catch((err) => {
+            console.log(err);
+            this.main();
+        });
+    }
 
+    search() {
+        inquirer.prompt(this.book.searchQuestions)
+            .then((target) => {
+                this.book.search(target.name)
+                    .then((contact) => {
+                        if (contact === null) {
+                            this.clear();
+                            console.log("Contact not found");
+                            this.search();
+                        } else {
+                            this.showContact(contact);
+                        }
+                    })
+            })
+            .catch((err) => {
+                console.log(err);
+                this.main();
+            });
+    }
+    showContact(contact) {
+        this._printContact(contact);
+        inquirer.prompt(this.showContactQuestions)
+            .then((answer) => {
+                switch (answer.selected) {
+                    case "Delete contact":
+                        this.delete(contact);
+                        break;
+                    case "Main menu":
+                        this.main();
+                        break;
+                    default:
+                        console.log("Something went wrong.");
+                        this.showContact(contact);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                this.showContact(contact);
+            });
+    }
+    _printContact(contact) {
+        console.log(`
+        name: ${contact.name}
+        phone number: ${contact.phone}
+        email: ${contact.email}
+        ---------------`
+        );
+    }
 
->>>>>>> prompt-menu-a
+    delete(contact) {
+        inquirer.prompt(this.book.deleteConfirmQuestions)
+            .then((answer) => {
+                if (answer.confirmation) {
+                    this.book.delete(contact.id);
+                    console.log("contact deleted!");
+                    this.main();
+                } else {
+                    console.log("contact not deleted");
+                    this.showContact(contact);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                this.main();
+            });
+    }
+
 }
